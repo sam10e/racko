3a66d6f5fcdc457e3522b21e428ff758bc91e7ae
Sam Tenney <samuelj10e@gmail.com>
2018-10-17 20:49:08 -0600

    Commented out square function code

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/*
+    HTML for square function
+    <form onsubmit="return square()">
+        <label>Input a number to find the square:</label>
+        <input type="number" id="userInput">
+    </form>
+    <input type="submit" onclick="square()">
+    <h3>Your number squared is: <span id="result"></span></h3>
+
+// JavaScript for square function
 function square() {
     var input = document.getElementById("userInput").value;
     console.log(input);
