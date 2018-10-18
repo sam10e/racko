9e09e39eba17186b6da6529bc9de039b34b26d7c
Sam Tenney <samuelj10e@gmail.com>
2018-10-17 20:46:55 -0600

    Finished warm up of finding the square of an input

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-function square(num) {
-    return num * num;
-};
+function square() {
+    var input = document.getElementById("userInput").value;
+    console.log(input);
+    var input_squared = input * input;
+    document.getElementById("result").innerHTML = input_squared; 
+}
 
-var results = document.getElementById("userInput").value;
 
-document.getElementById("result").innerHTML = results;
