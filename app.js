1d05e5de2c1e79c693b270e9a14869ac1d5c9568
Sam Tenney <samuelj10e@gmail.com>
2018-10-18 21:24:06 -0600

    Added startGame() function that creates deck of 60 cards and deals them to 2 players

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+function startGame() {
+    deck = [];
+    player1 = [];
+    player2 = [];
+    numCards = 60;
+    numHand = 10;
+    for(var i = 1; i <= numCards; i++) {
+        deck.push(i);
+    }
+    shuffle(deck);
+    for (var i = 0; i < numHand; i++) {
+        player1.push(deck.shift());
+        player2.push(deck.shift());
+    }
+    document.getElementById("player1").innerHTML = player1;
+    document.getElementById("player2").innerHTML = player2;
+    document.getElementById("deck").innerHTML = deck;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
     HTML for square function
     <form onsubmit="return square()">
@@ -14,5 +63,4 @@ function square() {
     var input_squared = input * input;
     document.getElementById("result").innerHTML = input_squared; 
 }
-
-
+*/
